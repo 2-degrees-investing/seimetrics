24b16a13fa955c627d0f07f9ae62e1176e26b20b
jbglattfelder <jbglattfelder@users.noreply.github.com>
2017-01-09 18:25:47 +0100

    Update fGDPowerDatatClean.r

--- code/fGDPowerDatatClean.r
+++ code/fGDPowerDatatClean.r
@@ -1,1 +1,9 @@
-xxx
+# Formatting
+for(i in 1:ncol(GDmaster2)) {
+	if (typeof(GDmaster2[,i]) == 'character') {
+    GDmaster[,i] <- str_replace_all(GDmaster2[,i], "\n", "") # linux
+	} 
+	if (i == 2 || i == 3) {
+    GDmaster[,i] <- iconv(GDmaster2[,i], "", "ASCII", "byte") #DEFAULT
+	}
+}
