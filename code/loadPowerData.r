fd5c3e21f73650bdc95255d5f8066467d68e37ec
jbglattfelder <jbg@jth.ch>
2017-01-10 17:46:06 +0100

    R code

--- code/loadPowerData.r
+++ code/loadPowerData.r
@@ -20,8 +20,31 @@ setwd("/home/jbg/work2/SEIMetrics/fresh011216/github/seimetrics/code/")
 # setwd("...") 
 
 # Load plant level GD data
-GDmaster <- read.csv("AprilDatabase2.csv",stringsAsFactors=FALSE,strip.white=TRUE)
-GDmaster2 <- GDmaster
+GDmaster <- read.csv("AprilDatabase2-f.csv",stringsAsFactors=FALSE,strip.white=TRUE)
 
-### Part 1:
+
+###############
+### Part 1: Data cleaning
+# INPUT: Data frame: GDmaster
 source("fGDPowerDatatClean.r")
+# Output: Saved CSV: GDataTotalsbyStatus.csv (totals by status, derive using active and pipeline),
+# 		  GDmasterclean.csv (cleaned version of GDmaster)
+
+# Test
+totcaptest <- sum(GDmaster$Total.Capacity..MW.[!is.na(GDmaster$Total.Capacity..MW.)])
+cat("Test 1:")
+if (abs(totcaptest-9539050.56) < .Machine$double.eps) { 
+	### XXX WTF? totcaptest is 9539051
+	cat("OK")
+} else {
+	cat("ERROR")
+}
+totcaptest2 <- sum(totstat2$Active[!is.na(totstat2$Active)])
+cat("Test 2:")
+if (abs(totcaptest2-5606925.0-0.189) < .Machine$double.eps) { 
+	### XXX WTF? totcaptest is 5606925
+	cat("OK")
+} else {
+	cat("ERROR")
+}
+###############
