ad8134ed132671d54016a7b848adb8a487782252
James B Glattfelder <spam@jth.ch>
2017-01-11 10:35:27 +0100

    Updating R scripts: loadPowerData

--- code/loadPowerData.r
+++ code/loadPowerData.r
@@ -15,18 +15,23 @@ library(scales)
 library(stringr)
 library(stringi)
 
+# Accuracy
+options(digits=10)
+
 # Set the path of the working directory
 setwd("/home/jbg/work2/SEIMetrics/fresh011216/github/seimetrics/code/") 
 # setwd("...") 
 
 # Load plant level GD data
+# Expects correctly formatted AprilDatabase2.csv called AprilDatabase2-f.csv: See seimetrics/data/ GitHub directory 
 GDmaster <- read.csv("AprilDatabase2-f.csv",stringsAsFactors=FALSE,strip.white=TRUE)
 
 
 ###############
 ### Part 1: Data cleaning
+
 # INPUT: Data frame: GDmaster
-source("fGDPowerDatatClean.r")
+source("fGDPowerDataCleaning.r")
 # Output: Saved CSV: GDataTotalsbyStatus.csv (totals by status, derive using active and pipeline),
 # 		  GDmasterclean.csv (cleaned version of GDmaster)
 
@@ -34,17 +39,16 @@ source("fGDPowerDatatClean.r")
 totcaptest <- sum(GDmaster$Total.Capacity..MW.[!is.na(GDmaster$Total.Capacity..MW.)])
 cat("Test 1:")
 if (abs(totcaptest-9539050.56) < .Machine$double.eps) { 
-	### XXX WTF? totcaptest is 9539051
 	cat("OK")
 } else {
 	cat("ERROR")
 }
 totcaptest2 <- sum(totstat2$Active[!is.na(totstat2$Active)])
 cat("Test 2:")
-if (abs(totcaptest2-5606925.0-0.189) < .Machine$double.eps) { 
-	### XXX WTF? totcaptest is 5606925
+if (abs(totcaptest2-5606925.189) < .Machine$double.eps) { 
 	cat("OK")
 } else {
 	cat("ERROR")
 }
+
 ###############
