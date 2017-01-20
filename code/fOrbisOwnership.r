b796ee9148037921e7143a2aa6dc7ddb585ccc65
James B Glattfelder <spam@jth.ch>
2017-01-20 11:22:31 +0100

    Hack to force ordering in ownerstruct; Orbis stub

---
+++ code/fOrbisOwnership.r
@@ -0,0 +1,16 @@
+### INPUT:
+
+
+### Output:
+
+# Load LookUp table with 129 ACWI utilities linking:
+# ACWI (ISIN), Orbis (BvDID), ProductionDataCompanyList (GDID, ISIN) and GDCompanyList (GDID)
+
+
+
+# Get 125 utilities found in ACWI, Orbis, ProductionDataCompanyList and GDCompanyList
+
+
+
+
+
